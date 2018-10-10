e84117bd68f85c30494de347ed1231ea6246b5d6
huadong <hd2260332898@gmail.com>
2018-10-10 18:24:14 +0800

    first commit

---
+++ src/main/java/com/ys/mongodb/MongodbApplication.java
@@ -0,0 +1,16 @@
+package com.ys.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HD
+ * @date 2018/10/10 18:18
+ */
+@SpringBootApplication
+public class MongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbApplication.class, args);
+    }
+}
